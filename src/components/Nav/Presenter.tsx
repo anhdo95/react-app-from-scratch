d5866e194a418877193bc9d520deb3afda95835f
anhdo <dinhanhdo95@gmail.com>
2019-10-05 22:37:20 +0700

    Add nav

--- src/components/Nav/Presenter.tsx
+++ src/components/Nav/Presenter.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretDown, faSearch } from '@fortawesome/free-solid-svg-icons'
 
 import './style.scss'
 
@@ -12,9 +14,33 @@ export default class Presenter extends React.Component<PresenterProps> {
 
 	render() {
 		return (
-			<div>
-				Hello World!
-			</div>
+			<nav className="nav">
+        <ul className="nav__links">
+          <li className="nav__link">
+            <a className="nav__anchor" href="/">Home</a>
+          </li>
+          <li className="nav__link">
+            <a className="nav__anchor" href="/">Brand</a>
+          </li>
+          <li className="nav__link">
+            <a className="nav__anchor" href="/">Tour</a>
+          </li>
+          <li className="nav__link">
+            <a className="nav__anchor" href="/">Contact</a>
+          </li>
+          <li className="nav__link">
+            <a className="nav__anchor">More <FontAwesomeIcon icon={faCaretDown} /></a>
+            <ul className="nav__dropdown">
+              <li><a href="/">Merchandise</a></li>
+              <li><a href="/">Merchandise</a></li>
+              <li><a href="/">Merchandise</a></li>
+            </ul>
+          </li>
+        </ul>
+        <div className="nav__right">
+          <a href="/"><FontAwesomeIcon icon={faSearch} /></a>
+        </div>
+      </nav>
 		)
 	}
 }
