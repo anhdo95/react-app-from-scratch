e495ea70b618907b9a5f113cbcdfe572583a0fa8
anhdo <dinhanhdo95@gmail.com>
2019-10-06 11:37:52 +0700

    Update styles and markup for Header

--- src/components/Header/Presenter.tsx
+++ src/components/Header/Presenter.tsx
@@ -7,14 +7,20 @@ interface PresenterProps {
 }
 
 export default class Presenter extends React.Component<PresenterProps> {
-	componentDidMount() {
-	}
+  componentDidMount() {
+  }
 
-	render() {
-		return (
-			<div>
-				Hello World!
-			</div>
-		)
-	}
+  render() {
+    return (
+      <header className="rel header">
+        <figure className="header__figure">
+          <img className="abs-full header__image" src="/assets/images/basic-design.jpg" alt=""/>
+          <section className="abs abs-horizontal-center align-c header__content">
+            <h2 className="sub-heading header__city">Chiago</h2>
+            <p className="description header__description">Thank you, Chiago - A night we won’t forget.</p>
+          </section>
+        </figure>
+      </header>
+    )
+  }
 }
